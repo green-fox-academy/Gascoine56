da3f312299730fc2dad6266572cafe1c45756284
Palo <pavolrehak16@gmail.com>
2021-09-15 06:28:39 +0200

    Commiting TicTacToe solution

--- week-02/day-02/Exercises/src/TicTacToe.java
+++ week-02/day-02/Exercises/src/TicTacToe.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        Path x = Path.of("src/owin.txt");
+        Path x = Path.of("src/draw.txt");
 
         try {
             ticTacResult(x);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Could not read file");;
         }
 
     }
@@ -33,7 +33,23 @@ public class TicTacToe {
         if(list.get(0).startsWith("O") && list.get(1).startsWith("O") && list.get(2).startsWith("O")){
             System.out.println("O wins!");
         }
-        else if()
+        else if(list.get(0).startsWith("X") && list.get(1).startsWith("X") && list.get(2).startsWith("X")){
+            System.out.println("X wins!");}
+        else if(list.get(0).endsWith("X") && list.get(1).endsWith("X") && list.get(2).endsWith("X")){
+            System.out.println("X wins!");}
+        else if(list.get(0).endsWith("O") && list.get(1).endsWith("O") && list.get(2).endsWith("O")){
+            System.out.println("O wins!");}
+        else if(list.get(0).charAt(0) == 'O' && list.get(0).charAt(0) == list.get(1).charAt(1)
+                && list.get(2).charAt(2) == list.get(0).charAt(0) && list.get(1).charAt(1) == list.get(2).charAt(2)){
+            System.out.println("O wins!");}
+        else if(list.get(0).charAt(0) == 'X' && list.get(0).charAt(0) == list.get(1).charAt(1)
+                && list.get(2).charAt(2) == list.get(0).charAt(0) && list.get(1).charAt(1) == list.get(2).charAt(2)){
+            System.out.println("X wins!");}
+        else{
+            System.out.println("Draw");
+        }
+
+
 
 
 
